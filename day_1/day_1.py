1491e06dd541aa967ba3f5f11ad2e794f78dcbff
kostas <konstantinosblatsoukasrepo@gmail.com>
2019-04-26 13:50:04 +0300

    refactor(day_1) duplication removal

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,19 +1,10 @@
-file = open('input.txt')
-numbers = file.read()
-file.close()
-
-
 def captcha(numbers):
     final_numbers = []
     total_numbers = len(numbers)
 
     for index in range(0, total_numbers):
-        if index == total_numbers - 1:
-            current_number = numbers[index]
-            next_number = numbers[0]
-        else:
-            current_number = numbers[index]
-            next_number = numbers[index + 1]
+        current_number = numbers[index]
+        next_number = get_next_number(index, total_numbers, numbers)
 
         if current_number == next_number:
             final_numbers.append(int(current_number))
@@ -21,9 +12,20 @@ def captcha(numbers):
     return sum(final_numbers)
 
 
+def get_next_number(index, total_numbers, numbers):
+    if index == total_numbers - 1:
+        return numbers[0]
+    else:
+        return numbers[index + 1]
+
+
 assert captcha('1122') == 3
 assert captcha('1111') == 4
 assert captcha('1234') == 0
 assert captcha('91212129') == 9
+
+file = open('input.txt')
+numbers = file.read()
+file.close()
 
 print(captcha(numbers))
