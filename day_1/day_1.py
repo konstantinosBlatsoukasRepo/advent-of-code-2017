b8cf67721d573cae8655509bd7951fc695043512
kostas <konstantinosblatsoukasrepo@gmail.com>
2019-04-27 10:59:18 +0300

    add(day_1)  part 2

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,4 +1,4 @@
-def captcha(numbers):
+def part_1(numbers):
     final_numbers = []
     total_numbers = len(numbers)
 
@@ -19,13 +19,37 @@ def get_next_number(index, total_numbers, numbers):
         return numbers[index + 1]
 
 
-assert captcha('1122') == 3
-assert captcha('1111') == 4
-assert captcha('1234') == 0
-assert captcha('91212129') == 9
+def part_2(numbers):
+    total_numbers = len(numbers)
+
+    upper_bound = int(total_numbers / 2)
+
+    final_numbers = []
+    for index in range(0, upper_bound):
+        current_number = int(numbers[index])
+        next_number = int(numbers[index + upper_bound])
+
+        if current_number == next_number:
+            final_numbers.append(current_number)
+            final_numbers.append(next_number)
+
+    return sum(final_numbers)
+
+
+assert part_2('1212') == 6
+assert part_2('1221') == 0
+assert part_2('123425') == 4
+assert part_2('123123') == 12
+assert part_2('12131415') == 4
+
+assert part_1('1122') == 3
+assert part_1('1111') == 4
+assert part_1('1234') == 0
+assert part_1('91212129') == 9
 
 file = open('input.txt')
 numbers = file.read()
 file.close()
 
-print(captcha(numbers))
+print(part_1(numbers))
+print(part_2(numbers))
