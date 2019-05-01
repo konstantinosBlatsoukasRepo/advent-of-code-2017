adb8f768452648d8deae147435dfe2fb86c1b934
kostas <konstantinosblatsoukasrepo@gmail.com>
2019-05-01 13:05:32 +0300

    add(day_4) refactor

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -15,7 +15,7 @@ def parse(file_name):
     return parsed_rows
 
 
-def count_valid(passphrases):
+def count_valid(passphrases, is_valid):
     total_valid_passphrases = 0
     for passphrase in passphrases:
         if is_valid(passphrase[0]):
@@ -23,15 +23,26 @@ def count_valid(passphrases):
     return total_valid_passphrases
 
 
+def are_valid(words_occurrences):
+    for value in words_occurrences:
+        if value != 1:
+            return False
+    return True
+
+
 def is_valid(passphrase):
     words = passphrase.split()
     counted_words = collections.Counter(words)
+    words_occurrences = counted_words.values()
+    return are_valid(words_occurrences)
 
-    for value in counted_words.values():
-        if value != 1:
-            return False
 
-    return True
+def is_valid_enhanced(passphrase):
+    words = passphrase.split()
+    sorted_words = [''.join(sorted(word)) for word in words]
+    counted_words = collections.Counter(sorted_words)
+    words_occurrences = counted_words.values()
+    return are_valid(words_occurrences)
 
 
 # part one unit tests
@@ -39,38 +50,14 @@ assert is_valid("aa bb cc dd ee") == True
 assert is_valid("aa bb cc dd aa") == False
 assert is_valid("aa bb cc dd aaa") == True
 
-
 input = parse('input.txt')
-print(count_valid(input))
+print("Part one result: {}".format(count_valid(input, is_valid)))
 
-
-def is_valid_enhanced(passphrase):
-    words = passphrase.split()
-    sorted_words = [''.join(sorted(word)) for word in words]
-
-    counted_words = collections.Counter(sorted_words)
-
-    for value in counted_words.values():
-        if value != 1:
-            return False
-
-    return True
-
-
-# part 2 uni tests
+# part 2 unit tests
 assert is_valid_enhanced("abcde fghij") == True
 assert is_valid_enhanced("abcde xyz ecdab") == False
 assert is_valid_enhanced("a ab abc abd abf abj") == True
 assert is_valid_enhanced("iiii oiii ooii oooi oooo") == True
 assert is_valid_enhanced("oiii ioii iioi iiio") == False
 
-
-def count_enhanced_valid(passphrases):
-    total_valid_passphrases = 0
-    for passphrase in passphrases:
-        if is_valid_enhanced(passphrase[0]):
-            total_valid_passphrases += 1
-    return total_valid_passphrases
-
-
-print(count_enhanced_valid(input))
+print("Part two result: {}".format(count_valid(input, is_valid_enhanced)))
