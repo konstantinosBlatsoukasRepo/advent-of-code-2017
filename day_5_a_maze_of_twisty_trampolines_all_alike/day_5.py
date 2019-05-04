89c1f39954d1d22ad2b80566350c148d4dc7dbfe
kostas <konstantinosblatsoukasrepo@gmail.com>
2019-05-04 11:47:57 +0300

    add(day_5) part 2

--- day_5_a_maze_of_twisty_trampolines_all_alike/day_5.py
+++ day_5_a_maze_of_twisty_trampolines_all_alike/day_5.py
@@ -13,14 +13,36 @@ def steps_to_reach_exit(jump_offsets):
         previous_index = next_index
         try:
             next_index = previous_index + jump_offsets[next_index]
+            jump_offsets[previous_index] += 1
             step_counter += 1
         except IndexError:
             return step_counter
-
-        jump_offsets[previous_index] += 1
 
 
 assert steps_to_reach_exit([0, 3, 0, 1, -3]) == 5
 
 jump_offsets = parse('input.txt')
 print(steps_to_reach_exit(jump_offsets))
+
+
+def strange_steps_to_reach_exit(jump_offsets):
+    step_counter = 0
+    next_index = 0
+
+    while True:
+        previous_index = next_index
+        try:
+            next_index = previous_index + jump_offsets[next_index]
+            if jump_offsets[previous_index] > 2:
+                jump_offsets[previous_index] -= 1
+            else:
+                jump_offsets[previous_index] += 1
+            step_counter += 1
+        except IndexError:
+            return step_counter
+
+
+assert strange_steps_to_reach_exit([0, 3, 0, 1, -3]) == 10
+
+jump_offsets = parse('input.txt')
+print(strange_steps_to_reach_exit(jump_offsets))
