de3050916e7af2b8d793f1e504e5be44d28107d2
kostas <konstantinosblatsoukasrepo@gmail.com>
2019-06-01 19:32:11 +0300

    add(day12_part2)

--- day_12_digital_plumber/day_12.py
+++ day_12_digital_plumber/day_12.py
@@ -36,26 +36,45 @@ def count_programs_connected_to_zero(programs, connected_to_zero):
 file = open('day12_input.txt')
 input = file.read()
 
+# input = """
+# 0 <-> 2
+# 1 <-> 1
+# 2 <-> 0, 3, 4
+# 3 <-> 2, 4
+# 4 <-> 2, 3, 6
+# 5 <-> 6
+# 6 <-> 4, 5
+# """
 
 programs = parse(input)
-connected_to_zero = {0}
-connected_to_zero, re_check_programs = count_programs_connected_to_zero(
-    programs, connected_to_zero)
+
+connected_ids = set()
+total_groups = 0
+
+for program_id in range(0, len(programs)):
+    print(program_id)
+    if program_id not in connected_ids:
+        connected_to_zero = {program_id}
+        connected_to_zero, re_check_programs = count_programs_connected_to_zero(
+            programs, connected_to_zero)
+
+        re_check_programs = {
+            program_id for program_id in re_check_programs if program_id not in connected_to_zero}
+
+        to_re_check = []
+        for program_id, can_talk_to in programs:
+            if program_id not in connected_to_zero and program_id in re_check_programs:
+                to_re_check.append((program_id, can_talk_to))
+
+        for _ in range(0, len(to_re_check)):
+            connected_to_zero, re_check_programs = count_programs_connected_to_zero(
+                programs, connected_to_zero)
+
+        if connected_to_zero != {}:
+            total_groups += 1
+
+        for connected in connected_to_zero:
+            connected_ids.add(connected)
 
 
-re_check_programs = {
-    program_id for program_id in re_check_programs if program_id not in connected_to_zero}
-
-
-to_re_check = []
-for program_id, can_talk_to in programs:
-    if program_id not in connected_to_zero and program_id in re_check_programs:
-        to_re_check.append((program_id, can_talk_to))
-
-
-for program in to_re_check:
-    connected_to_zero, re_check_programs = count_programs_connected_to_zero(
-        programs, connected_to_zero)
-
-
-print(len(connected_to_zero))
+print(total_groups)
